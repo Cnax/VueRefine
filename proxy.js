793f66cc71ccdfe0e67e7b29865576a6002eb80e
a77321a <1817169988@qq.com>
2020-07-18 09:31:33 +0800

    改写方法

--- proxy.js
+++ proxy.js
@@ -3,29 +3,36 @@
  * @Author: a77321a
  * @Date: 2020-07-15 13:59:09
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-07-15 14:35:53
+ * @LastEditTime: 2020-07-15 15:10:07
  */
 const data = {
   name: 'aaa',
   age: 20
 }
 // const data = [1,2]
-const proxyData = new Proxy(data, {
-  get (target, key, rec) {
-    // 只处理实例的属性
-    const ownKeys = Reflect.Reflect(target)
-    if (ownKeys.includes(key)) { }
-    const res = Reflect.get(target, key, rec)
-    return res
-  },
-  set (target, key, val, rec) {
-    const oldVal = target[key]
-    if (oldVal === val) { return true }
-    const res = Reflect.set(target, key, val, rec)
-    return res
-  },
-  deleteProperty (target, key) {
-    const res = Reflect.deleteProperty(target, key)
-    return res
+const defineReactive = (data = {}) => {
+  if (typeof data !== 'object' || data == null) {
+    return data
   }
-})
+  const proxyConfig = {
+    get (target, key, rec) {
+      // 只处理实例的属性
+      const ownKeys = Reflect.Reflect(target)
+      if (ownKeys.includes(key)) { }
+      const res = Reflect.get(target, key, rec)
+      // 深度监听
+      return deReactive(res)
+    },
+    set (target, key, val, rec) {
+      const oldVal = target[key]
+      if (oldVal === val) { return true }
+      const res = Reflect.set(target, key, val, rec)
+      return res
+    },
+    deleteProperty (target, key) {
+      const res = Reflect.deleteProperty(target, key)
+      return res
+    }
+  }
+  return new Proxy(data, proxyConfig)
+}
